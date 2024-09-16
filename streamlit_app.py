ce74e111c51f916342d756be353acbea9144c46b
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 22:09:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ with st.form(key='know_more_form'):
 height = height * height
 bmi = weight / height
 if bmi >= 40.0:
-  print("You are obese")
+  st.switch_page("pages/obesefile.py")
 elif bmi >= 25.0:
   print("You are overweight")
 elif bmi >=18.5:
