2b79d847ba46565b1458492db59cb7e87dfd148a
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 21:52:53 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,16 @@ with st.form(key='know_more_form'):
   st.header("This is a form to get to know more about you!")
   gender = st.selectbox("What is your gender?",["Female","Male"])
   age = st.text_input("Which year are you born?")
-  height = st.text_input("What is your height?(in meters)")
-  weight = st.text_input("What is your weight? (in kilograms)")
+  float(height) = st.text_input("What is your height? (in meters)")
+  float(weight) = st.text_input("What is your weight? (in kilograms)")
   st.form_submit_button("Submit")
-
+height = height * height
+bmi = weight / height
+if bmi >= 40.0:
+  print("You are obese")
+elif bmi >= 25.0:
+  print("You are overweight")
+elif bmi >=18.5:
+  print("You are normal")
+else:
+  print("You are underweight")
