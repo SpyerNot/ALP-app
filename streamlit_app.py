153ba479fd483b8c5261902abcbf1c1e54350a3f
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 21:36:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ gender = st.selectbox("What is your gender?",["Female","Male"])
 age = st.text_input("Which year are you born?")
 height = st.text_input("What is your height?")
 weight = st.text_input("What is your weight?")
+st.form_submit_button("This is a form to know more about you!")
 
