b3c01d6dc24230057dc589f9077d5038495de8c6
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 21:41:34 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.title("Our Health App!!")
-with st.form(key='Get to know more about you!'):
+st.header("This is a form to get to know more about you!")
+with st.form(key='know_more_form'):
+  st.header("This is a form to get to know more about you!")
   gender = st.selectbox("What is your gender?",["Female","Male"])
   age = st.text_input("Which year are you born?")
   height = st.text_input("What is your height?")
