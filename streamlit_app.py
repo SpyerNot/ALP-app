c8c7fdb34ae5eb461289b543869808e83fac9384
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 21:30:45 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Our Health App!!")
+st.selectbox("What is your gender?",["Female","Male"])
