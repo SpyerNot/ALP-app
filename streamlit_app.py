cb87387b6e433bc6e52dc895899c8d7514004d80
SpyerNot <low_li_wen@students.edu.sg>
2024-09-16 22:03:18 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ with st.form(key='know_more_form'):
   st.header("This is a form to get to know more about you!")
   gender = st.selectbox("What is your gender?",["Female","Male"])
   age = st.text_input("Which year are you born?")
-  height = st.text_input("What is your height? (in meters)")
-  weight = st.text_input("What is your weight? (in kilograms)")
+  height = st.number_input("What is your height?",0.00,2.01)
+  weight = st.number_input("What is your weight?",0.00,200.1)
   st.form_submit_button("Submit")
 height = height * height
 bmi = weight / height
