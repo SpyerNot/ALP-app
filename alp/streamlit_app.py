390740cd3b5599e5976f869821fccd4249439354
SpyerNot <low_li_wen@students.edu.sg>
2024-09-17 11:42:49 +0800

    Update streamlit_app.py

--- alp/streamlit_app.py
+++ alp/streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from page_functions import page1
 st.set_page_config(page_title="Welcome")
-pg = st.navigation([st.Page(page1),st.Page("rewards.py")]
+pg = st.navigation([st.Page(page1), st.Page("rewards.py")]
+pg.run()
 st.title("Our Health App!!")
 with st.form(key='know_more_form'):
   st.header("This is a form to get to know more about you!")
